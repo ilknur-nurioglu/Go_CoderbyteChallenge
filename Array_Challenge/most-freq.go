1ba62134b4fc53184dd77cc33d2796375bfbe84b
İlknur Nurioğlu <97106063+ilknur-nurioglu@users.noreply.github.com>
2022-03-19 14:07:05 +0300

    most-freq.go

---
+++ Array_Challenge/most-freq.go
@@ -0,0 +1,17 @@
+// most frequent element in array
+package main
+
+func mostFrequent(arr []int) int { // assuming no tie
+	m := map[int]int{}
+	var maxCnt int
+	var freq int
+	for _, a := range arr {
+		m[a]++
+		if m[a] > maxCnt {
+			maxCnt = m[a]
+			freq = a
+		}
+	}
+
+	return freq
+}
